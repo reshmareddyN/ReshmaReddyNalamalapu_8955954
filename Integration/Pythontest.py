dd9d8948ce73a8cdee1ab282080edc64946b988b
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-02-14 17:30:31 -0500

    integration_test.py

---
+++ Integration/Pythontest.py
@@ -0,0 +1,9 @@
+# integration_test.py
+
+def test_integration_pass():
+    # Write the integration test case that should pass
+    assert 2 + 2 == 4
+
+def test_integration_fail():
+    # Write the integration test case that should fail
+    assert 2 * 2 == 5
