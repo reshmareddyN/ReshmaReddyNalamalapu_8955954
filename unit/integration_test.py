b8d354331cdede74b680ccaab94a6dc057c55362
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-03-11 15:36:39 -0400

    Assignment3

--- unit/integration_test.py
+++ unit/integration_test.py
@@ -1,33 +1,9 @@
-# import addition
-# import multiplication
-import calculator
-def test_integration():
-    # Test multiplying the result of adding two numbers with another number
-    assert calculator.multiply(calculator.add(2, 3), 4) == 20
+# integration_test.py
 
+def test_integration_pass():
+    # Write the integration test case that should pass
+    assert 2 + 2 == 4
 
-
-## Unit testing
-
-# def test_addition():
-#     assert calculator.add(2, 3) == 5
-#
-# def test_subtraction():
-#     assert calculator.subtract(5, 3) == 2
-#
-# def test_multiplication():
-#     assert calculator.multiply(2, 3) == 6
-#
-# def test_division():
-#     assert calculator.divide(6, 3) == 2
-#
-# def test_divide_by_zero():
-#     try:
-#         calculator.divide(6, 0)
-#     except ValueError as e:
-#         assert str(e) == "Cannot divide by zero"
-#     else:
-#         assert False, "Expected ValueError"
-#
-#
-#
+def test_integration_fail():
+    # Write the integration test case that should fail
+    assert 2 * 2 == 5
